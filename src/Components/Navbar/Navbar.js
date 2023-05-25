c0974e219f9f971d147a9ccfbea8e5aee5d5c496
tachyontechs <hafizabdullahqurashi1@gmail.com>
2023-05-25 05:31:46 +0500

    update

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import './Navbar.scss'
+import logo from '../../assets/logo.png'
+function Navbar() {
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+  return (
+   <div className="navbar-parent">
+     <nav className="nav">
+      <a href="#" className="nav__brand">
+       <img src={logo} alt="" width={'40%'} className="logo" />
+      </a>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="#" className="nav__link active">
+            My Jobs
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            About
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Portfolio
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Skills
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact
+          </a>
+        </li>
+        {/* <button>Get Started</button> */}
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+   </div>
+  );
+}
+
+export default Navbar;
